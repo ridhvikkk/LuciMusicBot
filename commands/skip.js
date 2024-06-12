7a73f5bc3bac667ead1b38641bc467ccc3d2f626
ridhvikkk <tanmayshikha@gmail.com>
2024-06-12 23:35:21 +0530

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -67,7 +67,6 @@ try {
     .setAuthor({
       name: 'Song Skipped',
       iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
-      url: 'https://discord.gg/FUEHs7RCqz'
     })
     .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
     .setTimestamp();
